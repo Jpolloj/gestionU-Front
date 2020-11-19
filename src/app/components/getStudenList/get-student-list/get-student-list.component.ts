ea9ffbbb362827c0dd7bd15b74770a0e8b51b1a4
Jhon Jairo Castillo Martinez <jcast57@192.168.1.105>
2020-11-19 11:43:48 -0500

    Se implementa listado Collage

--- src/app/components/getStudenList/get-student-list/get-student-list.component.ts
+++ src/app/components/getStudenList/get-student-list/get-student-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GetCollageListService } from 'src/app/providers/getCollageList/get-collage-list.service';
 import { GetStudentListService } from 'src/app/providers/getStudentList/get-student-list.service';
 
 @Component({
@@ -8,15 +9,22 @@ import { GetStudentListService } from 'src/app/providers/getStudentList/get-stud
 })
 export class GetStudentListComponent implements OnInit {
 
-  constructor(private readonly getStudentListService: GetStudentListService) { }
+  constructor(private readonly getStudentListService: GetStudentListService,
+              private readonly getCollageListService: GetCollageListService) { }
 
 
   public responseData: any;
+  public responseCollage: any;
 
   ngOnInit() {
     this.getStudentListService.getStudentList().subscribe( (data) => {
       console.log(data);
       this.responseData = data;
+    });
+
+    this.getCollageListService.getStudentList().subscribe( (data) => {
+      console.log(data);
+      this.responseCollage = data;
     });
   }
 
